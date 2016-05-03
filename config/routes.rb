936b832969b939d56d6a760763a78d5551af71cf
vsathpathy <vsathpathy2017@kellogg.northwestern.edu>
2016-05-03 13:56:45 -0500

    75% done
    Request Uri too large error TBD

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,15 @@ Rails.application.routes.draw do
   # Routes for the Director resource:
   # CREATE
   get('/directors/new_form', { :controller => 'directors', :action => 'new_form' })
-  get('/create_director', { :controller => 'director', :action => 'create_row' })
+  get('/create_director', { :controller => 'directors', :action => 'create_row' })
 
   # READ
   get('/directors', { :controller => 'directors', :action => 'index' })
-  get('/directors/id', { :controller => 'directors', :action => 'show' })
+  get('/directors/:id', { :controller => 'directors', :action => 'show' })
 
   # UPDATE
   get('/directors/:id/edit_form', { :controller => 'directors', :action => 'edit_form' })
+  #check - Resource URI too large error -- parameter bio is too long
   get('/update_director/:id', { :controller => 'directors', :action => 'update_row' })
 
   # DELETE
