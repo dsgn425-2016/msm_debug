f671b6ef2f72dacdade35aef3f538cd835586048
Ahalya Vijay <avijay2017@users.noreply.github.com>
2016-05-04 18:15:30 -0500

    Movies debugged

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,46 +1,48 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    all_movies = Movie.all
+    movies_reverse = all_movies.reverse
+    @movies = movies_reverse
   end
 
   def show
-    @movie = Movie.find(params[id])
+    @movie = Movie.find_by({ :id => params[:id]})
   end
 
   def new_form
   end
 
   def create_row
-    @movie.title = params[:the_title]
-    @movie.year = params[:the_year]
-    @movie.duration = params[:the_duration]
-    @movie.description = params[:the_description]
-    @movie.image_url = params[:the_image_url]
-    @movie.director_id = params[:the_director_id]
-
-    @movie.save
-
-    render("show")
+    m = Movie.new
+    m.title = params[:title]
+    m.year = params[:year]
+    m.duration = params[:duration]
+    m.description = params[:description]
+    m.image_url = params[:image_url]
+    m.director_id = params[:director_id]
+    m.save
+    redirect_to("/movies")
   end
 
   def edit_form
-    @movie = Movie.find(params[:id])
+    @movie = Movie.find_by({ :id => params[:id]})
   end
 
   def update_row
+    @movie = Movie.find_by({ :id => params[:id]})
     @movie.title = params[:title]
     @movie.year = params[:year]
     @movie.duration = params[:duration]
     @movie.description = params[:description]
     @movie.image_url = params[:image_url]
     @movie.director_id = params[:director_id]
-
-    render("show")
+    @movie.save
+    render("show.html.erb")
   end
 
   def destroy
-    movie = Movie.find(params[:id])
-
-    movie.destroy
+    m = Movie.find_by({ :id => params[:id]})
+    @movie = m
+    m.destroy
   end
 end
