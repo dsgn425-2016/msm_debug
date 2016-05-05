e7e25f488de5674a42a163fb277ef7ea84a19ed1
nalonso2017 <nalonso2017@kellogg.northwestern.edu>
2016-05-04 23:51:43 -0500

    V1

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,13 +1,16 @@
 class DirectorsController < ApplicationController
   def index
-    directors = Director.all
+    @directors = Director.all
   end
 
   def show
-    @director = Director.find(params[:id])
+    @director = Director.find_by ({ :id => params[:id]})
+
+    render("show_details.html.erb")
   end
 
-  def new
+  def new_form
+    render("new_form.html.erb")
   end
 
   def create_row
@@ -19,29 +22,33 @@ class DirectorsController < ApplicationController
 
     @director.save
 
-    render("show")
+    render("show_details.html.erb")
   end
 
   def edit_form
-    @director = Director.find(params[:id])
+    @director = Director.find_by ({ :id => params[:id]})
+
+    render("edit_form.html.erb")
   end
 
   def update_row
-    @director = Director.find(params[:id])
+    @director = Director.find_by ({ :id => params[:id]})
 
-    @director.dob = params[":dob"]
-    @director.name = params[":name"]
-    @director.bio = params[":bio"]
-    @director.image_url = params[":image_url"]
+    @director.dob = params[:the_dob]
+    @director.name = params[:the_name]
+    @director.bio = params[:the_bio]
+    @director.image_url = params[:the_image_url]
 
     @director.save
 
-    render("show")
+    render("show_details.html.erb")
   end
 
   def destroy
-    @director = Director.find(params[:id])
+    @director = Director.find_by ({ :id => params[:id]})
 
     @director.destroy
+
+    render("destroy.html.erb")
   end
 end
