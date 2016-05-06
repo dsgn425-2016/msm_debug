9a2a010fdbace6536da75d28168635199b53d6a1
Ahalya Vijay <avijay2017@users.noreply.github.com>
2016-05-06 10:40:27 -0500

    Movies, Directors, Roles debug completed

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,13 +1,13 @@
 class DirectorsController < ApplicationController
   def index
-    directors = Director.all
+    @directors = Director.all
   end
 
   def show
-    @director = Director.find(params[:id])
+    @director = Director.find_by({ :id => params[:id]})
   end
 
-  def new
+  def new_form
   end
 
   def create_row
@@ -19,28 +19,28 @@ class DirectorsController < ApplicationController
 
     @director.save
 
-    render("show")
+    render("show.html.erb")
   end
 
   def edit_form
-    @director = Director.find(params[:id])
+    @director = Director.find_by({ :id => params[:id]})
   end
 
   def update_row
-    @director = Director.find(params[:id])
+    @director = Director.find_by({ :id => params[:id]})
 
-    @director.dob = params[":dob"]
-    @director.name = params[":name"]
-    @director.bio = params[":bio"]
-    @director.image_url = params[":image_url"]
+    @director.dob = params[:dob]
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.image_url = params[:image_url]
 
     @director.save
 
-    render("show")
+    render("show.html.erb")
   end
 
   def destroy
-    @director = Director.find(params[:id])
+    @director = Director.find_by({ :id => params[:id]})
 
     @director.destroy
   end
