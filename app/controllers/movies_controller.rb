86648c80e38737118dd7d2a579902d6d11be522c
dhuffman2017 <dhuffman2017@kellogg.northwestern.edu>
2016-05-07 15:42:27 -0500

    Movies debug so far

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,16 +1,17 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    @movie = Movie.all
   end
 
   def show
-    @movie = Movie.find(params[id])
+    @movie = Movie.find_by({ :id => params[:id]})
   end
 
   def new_form
   end
 
   def create_row
+    @movie = Movie.new
     @movie.title = params[:the_title]
     @movie.year = params[:the_year]
     @movie.duration = params[:the_duration]
@@ -20,27 +21,28 @@ class MoviesController < ApplicationController
 
     @movie.save
 
-    render("show")
+    render("show.html.erb")
   end
 
   def edit_form
-    @movie = Movie.find(params[:id])
+    @movie = Movie.find_by({ :id => params[:id]})
   end
 
   def update_row
+    @movie = Movie.find_by({ :id => params[:id]})
     @movie.title = params[:title]
     @movie.year = params[:year]
     @movie.duration = params[:duration]
     @movie.description = params[:description]
     @movie.image_url = params[:image_url]
     @movie.director_id = params[:director_id]
-
+    @movie.save
     render("show")
   end
 
-  def destroy
-    movie = Movie.find(params[:id])
-
-    movie.destroy
+  def delete
+    @movie = Movie.find_by({ :id => params[:id]})
+    @movie.destroy
+    render("delete_movies")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 Rails.application.routes.draw do
   # Route for bare domain:
   get('/', { :controller => 'movies', :action => 'index' })
-
-  # Routes for the Director resource:
-  # CREATE
-  get('/directors/new_form', { :controller => 'directors', :action => 'new_form' })
-  get('/create_director', { :controller => 'director', :action => 'create_row' })
-
-  # READ
-  get('/directors', { :controller => 'directors', :action => 'index' })
-  get('/directors/id', { :controller => 'directors', :action => 'show' })
-
-  # UPDATE
-  get('/directors/:id/edit_form', { :controller => 'directors', :action => 'edit_form' })
-  get('/update_director/:id', { :controller => 'directors', :action => 'update_row' })
-
-  # DELETE
-  get('/delete_director/:id', { :controller => 'directors', :action => 'destroy' })
-  #------------------------------
+  #
+  # # Routes for the Director resource:
+  # # CREATE
+  # get('/directors/new_form', { :controller => 'directors', :action => 'new_form' })
+  # get('/create_director', { :controller => 'director', :action => 'create_row' })
+  #
+  # # READ
+  # get('/directors', { :controller => 'directors', :action => 'index' })
+  # get('/directors/:id', { :controller => 'directors', :action => 'show' })
+  #
+  # # UPDATE
+  # get('/directors/:id/edit_form', { :controller => 'directors', :action => 'edit_form' })
+  # get('/update_director/:id', { :controller => 'directors', :action => 'update_row' })
+  #
+  # # DELETE
+  # get('/delete_director/:id', { :controller => 'directors', :action => 'destroy' })
+  # #------------------------------
 
   # Routes for the Movie resource:
   # CREATE
@@ -33,40 +33,40 @@ Rails.application.routes.draw do
   get('/update_movie/:id', { :controller => 'movies', :action => 'update_row' })
 
   # DELETE
-  get('/delete_movie/:id', { :controller => 'movies', :action => 'destroy' })
+  get('/delete_movies/:id', { :controller => 'movies', :action => 'delete' })
   #------------------------------
 
   # Routes for the Role resource:
-  # CREATE
-  get('/roles/new_form', { :controller => 'roles', :action => 'new_form' })
-  get('/create_role', { :controller => 'roles', :action => 'create_row' })
-
-  # READ
-  get('/roles', { :controller => 'roles', :action => 'index' })
-  get('/roles/:id', { :controller => 'roles', :action => 'show' })
-
-  # UPDATE
-  get('/roles/:id/edit_form', { :controller => 'roles', :action => 'edit_form' })
-  get('/update_role/:id', { :controller => 'roles', :action => 'update_row' })
-
-  # DELETE
-  get('/delete_role/:id', { :controller => 'roles', :action => 'destroy' })
-  #------------------------------
-
-  # Routes for the Actor resource:
-  # CREATE
-  get('/actors/new_form', { :controller => 'actors', :action => 'new_form' })
-  get('/create_actor', { :controller => 'actors', :action => 'create_row' })
-
-  # READ
-  get('/actors', { :controller => 'actors', :action => 'index' })
-  get('/actors/:id', { :controller => 'actors', :action => 'show' })
-
-  # UPDATE
-  get('/actors/:id/edit_form', { :controller => 'actors', :action => 'edit_form' })
-  get('/update_actor/:id', { :controller => 'actors', :action => 'update_row' })
-
-  # DELETE
-  get('/delete_actor/:id', { :controller => 'actors', :action => 'destroy' })
-  #------------------------------
+  # # CREATE
+  # get('/roles/new_form', { :controller => 'roles', :action => 'new_form' })
+  # get('/create_role', { :controller => 'roles', :action => 'create_row' })
+  #
+  # # READ
+  # get('/roles', { :controller => 'roles', :action => 'index' })
+  # get('/roles/:id', { :controller => 'roles', :action => 'show' })
+  #
+  # # UPDATE
+  # get('/roles/:id/edit_form', { :controller => 'roles', :action => 'edit_form' })
+  # get('/update_role/:id', { :controller => 'roles', :action => 'update_row' })
+  #
+  # # DELETE
+  # get('/delete_role/:id', { :controller => 'roles', :action => 'destroy' })
+  # #------------------------------
+  #
+  # # Routes for the Actor resource:
+  # # CREATE
+  # get('/actors/new_form', { :controller => 'actors', :action => 'new_form' })
+  # get('/create_actor', { :controller => 'actors', :action => 'create_row' })
+  #
+  # # READ
+  # get('/actors', { :controller => 'actors', :action => 'index' })
+  # get('/actors/:id', { :controller => 'actors', :action => 'show' })
+  #
+  # # UPDATE
+  # get('/actors/:id/edit_form', { :controller => 'actors', :action => 'edit_form' })
+  # get('/update_actor/:id', { :controller => 'actors', :action => 'update_row' })
+  #
+  # # DELETE
+  # get('/delete_actor/:id', { :controller => 'actors', :action => 'destroy' })
+  # #------------------------------
 end
