59e191defabb740fffcab4a061b7cbc86ef42614
jillseidel <jseidel2017@users.noreply.github.com>
2016-05-07 14:37:12 -0500

    directors debugged

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Rails.application.routes.draw do
   # Route for bare domain:
-  get('/', { :controller => 'movies', :action => 'index' })
+  get('/',                        { :controller => 'movies', :action => 'index' })
 
   # Routes for the Director resource:
   # CREATE
-  get('/directors/new_form', { :controller => 'directors', :action => 'new_form' })
-  get('/create_director', { :controller => 'director', :action => 'create_row' })
+  get('/directors/new_form',      { :controller => 'directors', :action => 'new_form' })
+  get('/create_director',         { :controller => 'directors', :action => 'create_row' })
 
   # READ
-  get('/directors', { :controller => 'directors', :action => 'index' })
-  get('/directors/id', { :controller => 'directors', :action => 'show' })
+  get('/directors',               { :controller => 'directors', :action => 'index' })
+  get('/directors/:id',            { :controller => 'directors', :action => 'show' })
 
   # UPDATE
   get('/directors/:id/edit_form', { :controller => 'directors', :action => 'edit_form' })
-  get('/update_director/:id', { :controller => 'directors', :action => 'update_row' })
+  get('/update_director/:id',     { :controller => 'directors', :action => 'update_row' })
 
   # DELETE
-  get('/delete_director/:id', { :controller => 'directors', :action => 'destroy' })
+  get('/delete_director/:id',     { :controller => 'directors', :action => 'destroy' })
   #------------------------------
 
   # Routes for the Movie resource:
