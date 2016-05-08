ed92e9630762054696342cfa7efc596dbf7a17f9
mbhangley1 <mbhangley2017@kellogg.northwestern.edu>
2016-05-08 18:29:50 -0500

    Ending point 1
    Looks good.

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,9 +1,9 @@
 class DirectorsController < ApplicationController
   def index
-    directors = Director.all
+    @directors = Director.all
   end
 
-  def show
+  def show_details
     @director = Director.find(params[:id])
   end
 
@@ -16,10 +16,9 @@ class DirectorsController < ApplicationController
     @director.name = params[:name]
     @director.bio = params[:bio]
     @director.image_url = params[:image_url]
-
     @director.save
 
-    render("show")
+    render("show_details.html.erb")
   end
 
   def edit_form
@@ -28,12 +27,10 @@ class DirectorsController < ApplicationController
 
   def update_row
     @director = Director.find(params[:id])
-
-    @director.dob = params[":dob"]
-    @director.name = params[":name"]
-    @director.bio = params[":bio"]
-    @director.image_url = params[":image_url"]
-
+    @director.dob = params[:dob]
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.image_url = params[:image_url]
     @director.save
 
     render("show")
@@ -41,7 +38,7 @@ class DirectorsController < ApplicationController
 
   def destroy
     @director = Director.find(params[:id])
-
     @director.destroy
   end
+
 end
