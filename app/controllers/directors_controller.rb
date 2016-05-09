2716c190446b950dae4ee07e23e38a13020e6fe9
ss3055 <salmansomjee@gmail.com>
2016-05-09 17:09:08 -0500

    Completed debugging directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,6 +1,6 @@
 class DirectorsController < ApplicationController
   def index
-    directors = Director.all
+    @director = Director.all
   end
 
   def show
@@ -28,15 +28,14 @@ class DirectorsController < ApplicationController
 
   def update_row
     @director = Director.find(params[:id])
-
-    @director.dob = params[":dob"]
-    @director.name = params[":name"]
-    @director.bio = params[":bio"]
-    @director.image_url = params[":image_url"]
+    @director.dob = params[:dob]
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.image_url = params[:image_url]
 
     @director.save
 
-    render("show")
+    redirect_to("/directors/"+params[:id])
   end
 
   def destroy
