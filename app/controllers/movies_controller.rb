f59f7fc51dff209ab3e4037df74d2ead9d19ea22
Christen Love <clove2017@kellogg.northwestern.edu>
2016-05-09 13:48:11 -0500

    update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,21 +4,32 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params[id])
+    @movie = Movie.find(params[:id])
+    @movie_id = params[:id]
+    @movie_title = params[:title]
+    @movie_year = params[:year]
+    @movie_duration = params[:duration]
+    @movie_description = params[:description]
+    @movie_image_url = params[:image_url]
+    @movie_director_id = params[:director_id]
+
+    render("show")
   end
 
   def new_form
+
   end
 
   def create_row
-    @movie.title = params[:the_title]
-    @movie.year = params[:the_year]
-    @movie.duration = params[:the_duration]
-    @movie.description = params[:the_description]
-    @movie.image_url = params[:the_image_url]
-    @movie.director_id = params[:the_director_id]
-
-    @movie.save
+    @movie = Movie.new
+    @movie_id = params[:the_id]
+    @movie_title = params[:the_title]
+    @movie_year = params[:the_year]
+    @movie_duration = params[:the_duration]
+    @movie_description = params[:the_description]
+    @movie_image_url = params[:the_image_url]
+    @movie_director_id = params[:the_director_id]
+    @movie_save
 
     render("show")
   end
@@ -28,12 +39,13 @@ class MoviesController < ApplicationController
   end
 
   def update_row
-    @movie.title = params[:title]
-    @movie.year = params[:year]
-    @movie.duration = params[:duration]
-    @movie.description = params[:description]
-    @movie.image_url = params[:image_url]
-    @movie.director_id = params[:director_id]
+    @movie_id = params[:id]
+    @movie_title = params[:title]
+    @movie_year = params[:year]
+    @movie_duration = params[:duration]
+    @movie_description = params[:description]
+    @movie_image_url = params[:image_url]
+    @movie_director_id = params[:director_id]
 
     render("show")
   end
