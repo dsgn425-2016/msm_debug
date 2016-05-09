49f7a7da4f9b8159eee99fa277fb3a284d99cd64
janieesmith <smithje89@gmail.com>
2016-05-09 13:20:53 -0500

    almost debugged

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -7,6 +7,7 @@ class ActorsController < ApplicationController
     @actor = Actor.find(params[:id])
   end
 
+
   def new_form
   end
 
@@ -22,6 +23,7 @@ class ActorsController < ApplicationController
     render("show")
   end
 
+
   def edit_form
     @actor = Actor.find(params[:id])
   end
@@ -29,10 +31,10 @@ class ActorsController < ApplicationController
   def update_row
     @actor = Actor.find(params[:id])
 
-    @actor.dob = params[:dob]
-    @actor.name = params[:name]
-    @actor.bio = params[:bio]
-    @actor.image_url = params[:image_url]
+    @actor.dob = params[:the_dob]
+    @actor.name = params[:the_name]
+    @actor.bio = params[:the_bio]
+    @actor.image_url = params[:the_image_url]
 
     @actor.save
 
