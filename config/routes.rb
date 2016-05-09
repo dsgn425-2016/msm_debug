1df223489d192420d4c8269d48d96e9d8850d14a
FamaLJ <blongjohn2017@kellogg.northwestern.edu>
2016-05-09 13:01:41 -0500

    done

--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,18 @@ Rails.application.routes.draw do
   # Routes for the Director resource only:
   # CREATE
   get('/directors/new_form', { :controller => 'directors', :action => 'new_form' })
-  get('/create_director', { :controller => 'director', :action => 'create_row' })
+  get('/create_director', { :controller => 'directors', :action => 'create_row' })
 
   # READ
   get('/directors', { :controller => 'directors', :action => 'index' })
-  get('/directors/id', { :controller => 'directors', :action => 'show' })
+  get('/directors/:id', { :controller => 'directors', :action => 'show' })
 
   # UPDATE
-  get('/directors/:id/edit_form', { :controller => 'directors', :action => 'edit_form' })
-  get('/update_director/:id', { :controller => 'directors', :action => 'update_row' })
+  get('/directors/edit_form/:id', { :controller => 'directors', :action => 'edit_form' })
+  get('directors/update_row/:id', { :controller => 'directors', :action => 'update_row' })
 
   # DELETE
-  get('/delete_director/:id', { :controller => 'directors', :action => 'destroy' })
+  get('directors/destroy/:id', { :controller => 'directors', :action => 'destroy' })
   #------------------------------
 
   # Routes for the Movie resource:
